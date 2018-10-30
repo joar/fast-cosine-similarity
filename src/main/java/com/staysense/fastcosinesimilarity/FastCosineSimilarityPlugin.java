a08aba6016b0b8950fc88ea484d63573f31fe343
Joar Wandborg <joar@wandborg.se>
2018-10-30 11:26:25 +0100

    Reformat

--- src/main/java/com/staysense/fastcosinesimilarity/FastCosineSimilarityPlugin.java
+++ src/main/java/com/staysense/fastcosinesimilarity/FastCosineSimilarityPlugin.java
@@ -39,178 +39,178 @@ import org.elasticsearch.script.ScriptEngine;
 import org.elasticsearch.script.ScoreScript;
 
 
-public final class FastCosineSimilarityPlugin extends Plugin implements ScriptPlugin{
+public final class FastCosineSimilarityPlugin extends Plugin implements ScriptPlugin {
 
     @Override
     public ScriptEngine getScriptEngine(Settings settings, Collection<ScriptContext<?>> contexts) {
         return new FastCosineSimilarityEngine();
     }
 
-  private static class FastCosineSimilarityEngine implements ScriptEngine {
+    private static class FastCosineSimilarityEngine implements ScriptEngine {
 
-    //
-    //The normalized vector score from the query
-    //
-    double queryVectorNorm;
+        //
+        //The normalized vector score from the query
+        //
+        double queryVectorNorm;
 
-    @Override
-    public String getType() {
-        return "fast_cosine";
-    }
-
-    @Override
-    public <T> T compile(String scriptName, String scriptSource, ScriptContext<T> context, Map<String, String> params) {
-        if (context.equals(ScoreScript.CONTEXT) == false) {
-            throw new IllegalArgumentException(getType() + " scripts cannot be used for context [" + context.name + "]");
+        @Override
+        public String getType() {
+            return "fast_cosine";
         }
-        // we use the script "source" as the script identifier
-        if ("staysense".equals(scriptSource)) {
-            ScoreScript.Factory factory = (p, lookup) -> new ScoreScript.LeafFactory() {
-                // The field to compare against
-                final String field;
-                //Whether this search should be cosine or dot product
-                final Boolean cosine;
-                //The query embedded vector
-                final Object vector;
-                Boolean exclude;
-                //The final comma delimited vector representation of the query vector
-                double[] inputVector;
-                {
-                    if (p.containsKey("field") == false) {
-                        throw new IllegalArgumentException("Missing parameter [field]");
-                    }
 
-                    //Determine if cosine
-                    final Object cosineBool = p.get("cosine");
-                    cosine = cosineBool != null ? (boolean)cosineBool : true;
+        @Override
+        public <T> T compile(String scriptName, String scriptSource, ScriptContext<T> context, Map<String, String> params) {
+            if (context.equals(ScoreScript.CONTEXT) == false) {
+                throw new IllegalArgumentException(getType() + " scripts cannot be used for context [" + context.name + "]");
+            }
+            // we use the script "source" as the script identifier
+            if ("staysense".equals(scriptSource)) {
+                ScoreScript.Factory factory = (p, lookup) -> new ScoreScript.LeafFactory() {
+                    // The field to compare against
+                    final String field;
+                    //Whether this search should be cosine or dot product
+                    final Boolean cosine;
+                    //The query embedded vector
+                    final Object vector;
+                    Boolean exclude;
+                    //The final comma delimited vector representation of the query vector
+                    double[] inputVector;
 
-                    //Get the field value from the query
-                    field = p.get("field").toString();
-
-                    final Object excludeBool = p.get("exclude");
-                    exclude = excludeBool != null ? (boolean)cosineBool : true;
-
-                    //Get the query vector embedding
-                    vector = p.get("vector");
-
-                    //Determine if raw comma-delimited vector or embedding was passed
-                    if(vector != null) {
-                        final ArrayList<Double> tmp = (ArrayList<Double>) vector;
-                        inputVector = new double[tmp.size()];
-                        for (int i = 0; i < inputVector.length; i++) {
-                            inputVector[i] = tmp.get(i);
+                    {
+                        if (p.containsKey("field") == false) {
+                            throw new IllegalArgumentException("Missing parameter [field]");
                         }
-                    } else {
-                        final Object encodedVector = p.get("encoded_vector");
-                        if(encodedVector == null) {
-                            throw new IllegalArgumentException("Must have 'vector' or 'encoded_vector' as a parameter");
-                        }
-                        inputVector = Util.convertBase64ToArray((String) encodedVector);
-                    }
 
-                    //If cosine calculate the query vec norm
-                    if(cosine) {
-                        queryVectorNorm = 0d;
-                        // compute query inputVector norm once
-                        for (double v : inputVector) {
-                            queryVectorNorm += Math.pow(v, 2.0);
-                        }
-                    }
-                }
+                        //Determine if cosine
+                        final Object cosineBool = p.get("cosine");
+                        cosine = cosineBool != null ? (boolean) cosineBool : true;
 
-                @Override
-                public ScoreScript newInstance(LeafReaderContext context) throws IOException {
+                        //Get the field value from the query
+                        field = p.get("field").toString();
 
-                    return new ScoreScript(p, lookup, context) {
-                          Boolean is_value = false;
+                        final Object excludeBool = p.get("exclude");
+                        exclude = excludeBool != null ? (boolean) cosineBool : true;
 
-                          // Use Lucene LeafReadContext to access binary values directly.
-                          BinaryDocValues accessor = context.reader().getBinaryDocValues(field);
+                        //Get the query vector embedding
+                        vector = p.get("vector");
 
-                          @Override
-                          public void setDocument(int docId) {
-                              // advance has undefined behavior calling with a docid <= its current docid
-                              try {
-                                  accessor.advanceExact(docId);
-                                  is_value = true;
-                              } catch (IOException e) {
-                                  is_value = false;
-                              }
-                          }
-
-                          @Override
-                          public double execute() {
-
-                            //If there is no field value return 0 rather than fail.
-                            if (!is_value) return 0.0d;
-
-                            final int inputVectorSize = inputVector.length;
-                            final byte[] bytes;
-
-                            try {
-                                 bytes = accessor.binaryValue().bytes;
-                            } catch (IOException e) {
-                                 return 0d;
+                        //Determine if raw comma-delimited vector or embedding was passed
+                        if (vector != null) {
+                            final ArrayList<Double> tmp = (ArrayList<Double>) vector;
+                            inputVector = new double[tmp.size()];
+                            for (int i = 0; i < inputVector.length; i++) {
+                                inputVector[i] = tmp.get(i);
                             }
-
-
-                            final ByteArrayDataInput docVector = new ByteArrayDataInput(bytes);
-
-                            docVector.readVInt();
-
-                            final int docVectorLength = docVector.readVInt(); // returns the number of bytes to read
-
-                            if(docVectorLength != inputVectorSize * 8) {
-                                return 0d;
+                        } else {
+                            final Object encodedVector = p.get("encoded_vector");
+                            if (encodedVector == null) {
+                                throw new IllegalArgumentException("Must have 'vector' or 'encoded_vector' as a parameter");
                             }
+                            inputVector = Util.convertBase64ToArray((String) encodedVector);
+                        }
 
-                            final int position = docVector.getPosition();
+                        //If cosine calculate the query vec norm
+                        if (cosine) {
+                            queryVectorNorm = 0d;
+                            // compute query inputVector norm once
+                            for (double v : inputVector) {
+                                queryVectorNorm += Math.pow(v, 2.0);
+                            }
+                        }
+                    }
 
-                            final DoubleBuffer doubleBuffer = ByteBuffer.wrap(bytes, position, docVectorLength).asDoubleBuffer();
+                    @Override
+                    public ScoreScript newInstance(LeafReaderContext context) throws IOException {
 
-                            final double[] docVector = new double[inputVectorSize];
-                            doubleBuffer.get(docVector);
+                        return new ScoreScript(p, lookup, context) {
+                            Boolean is_value = false;
 
-                            double docVectorNorm = 0d;
-                            double score = 0d;
+                            // Use Lucene LeafReadContext to access binary values directly.
+                            BinaryDocValues accessor = context.reader().getBinaryDocValues(field);
 
-                            //calculate dot product of document vector and query vector
-                            for (int i = 0; i < inputVectorSize; i++) {
-          
-                                score += docVector[i] * inputVector[i];
-
-                                if(cosine)
-                                {
-                                  docVectorNorm += Math.pow(docVector[i], 2.0);
+                            @Override
+                            public void setDocument(int docId) {
+                                // advance has undefined behavior calling with a docid <= its current docid
+                                try {
+                                    accessor.advanceExact(docId);
+                                    is_value = true;
+                                } catch (IOException e) {
+                                    is_value = false;
                                 }
                             }
 
-                            //If cosine, calcluate cosine score
-                            if(cosine) {
+                            @Override
+                            public double execute() {
 
-                                if (docVectorNorm == 0 || queryVectorNorm == 0) return 0d;
+                                //If there is no field value return 0 rather than fail.
+                                if (!is_value) return 0.0d;
 
-                                score =  score / (Math.sqrt(docVectorNorm) * Math.sqrt(queryVectorNorm));
+                                final int inputVectorSize = inputVector.length;
+                                final byte[] bytes;
+
+                                try {
+                                    bytes = accessor.binaryValue().bytes;
+                                } catch (IOException e) {
+                                    return 0d;
+                                }
+
+
+                                final ByteArrayDataInput docVector = new ByteArrayDataInput(bytes);
+
+                                docVector.readVInt();
+
+                                final int docVectorLength = docVector.readVInt(); // returns the number of bytes to read
+
+                                if (docVectorLength != inputVectorSize * 8) {
+                                    return 0d;
+                                }
+
+                                final int position = docVector.getPosition();
+
+                                final DoubleBuffer doubleBuffer = ByteBuffer.wrap(bytes, position, docVectorLength).asDoubleBuffer();
+
+                                final double[] docVector = new double[inputVectorSize];
+                                doubleBuffer.get(docVector);
+
+                                double docVectorNorm = 0d;
+                                double score = 0d;
+
+                                //calculate dot product of document vector and query vector
+                                for (int i = 0; i < inputVectorSize; i++) {
+
+                                    score += docVector[i] * inputVector[i];
+
+                                    if (cosine) {
+                                        docVectorNorm += Math.pow(docVector[i], 2.0);
+                                    }
+                                }
+
+                                //If cosine, calcluate cosine score
+                                if (cosine) {
+
+                                    if (docVectorNorm == 0 || queryVectorNorm == 0) return 0d;
+
+                                    score = score / (Math.sqrt(docVectorNorm) * Math.sqrt(queryVectorNorm));
+                                }
+
+                                return score;
                             }
+                        };
+                    }
 
-                            return score;
-                          }
-                    };
-                }
-
-                @Override
-                public boolean needs_score() {
-                    return false;
-                }
-            };
-            return context.factoryClazz.cast(factory);
+                    @Override
+                    public boolean needs_score() {
+                        return false;
+                    }
+                };
+                return context.factoryClazz.cast(factory);
+            }
+            throw new IllegalArgumentException("Unknown script name " + scriptSource);
         }
-        throw new IllegalArgumentException("Unknown script name " + scriptSource);
-    }
 
-    @Override
-    public void close() {
-        // optionally close resources
+        @Override
+        public void close() {
+            // optionally close resources
+        }
     }
-  }
 }
